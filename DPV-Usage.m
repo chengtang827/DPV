e6615e551f96a3a17d547cb73cbd06bd4dc5d2b1
chengtang827 <chengtang827@gmail.com>
2017-08-21 11:52:03 +0800

    use block and comment

--- DPV-Usage.m
+++ DPV-Usage.m
@@ -6,14 +6,14 @@ cd 073004/site01/session03
 pf1 = performance('auto');
 % uses @performance/plot.m to plot the results
 plot(pf1)
-
+%%
 cd ../../../081004/site01/session01
 % uses @performance/performance.m to compute another performance object
 pf2 = performance('auto');
 % uses @performance/plot.m to plot the results
 figure
 plot(pf2)
-
+%%
 cd ../../..
 % uses @performance/plus.m to combine 2 performance objects
 pf = pf1 + pf2;
@@ -22,27 +22,27 @@ InspectGUI(pf)
 % or to plot the combined data
 InspectGUI(pf,'overallonly')
 InspectGUI(pf,'overallonly','sessionmeans')
-
+%%
 % now instead of creating the objects session by session, we will use ProcessDays to create all 
 % possible performance objects. And instead of loading pre-saved objects, we are going to recompute
 % both the performance object, and the eyestarget object, which is used to compute performance
 pfpop = ProcessDays(performance,'redolevels',5);
 InspectGUI(pfpop)
-
+%%
 % the directories that contained valid data for the performance objects can now be extracted to
 % create other objects
 nd = nptdata(pfpop);
 [nd,tipop] = ProcessDirs(nd,'Object','timing','redolevels',5);
 InspectGUI(tipop)
-
-You can create a plot with 2 objects using the following:
+%%
+%You can create a plot with 2 objects using the following:
 
 InspectGUI(pfpop,'addObjs',{tipop},'SP',[2 1])
-
-If you want to place them side-by-side, you can do the following:
+%%
+%If you want to place them side-by-side, you can do the following:
 
 InspectGUI(pfpop,'addObjs',{tipop},'SP',[1 2])
-
+%%
 cd 081004/site01/session01/eye
 et = eyestarget('auto','redolevels',5);
 cd ..
