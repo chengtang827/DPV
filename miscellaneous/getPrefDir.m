ab44632db103d58a7b93f206213bc859c996279f
cheng <chengtang827@gmail.com>
2017-09-08 16:32:42 +0800

    using different implementation for windows and linux

--- miscellaneous/getPrefDir.m
+++ miscellaneous/getPrefDir.m
@@ -1,9 +1,20 @@
-function ans = getPrefDir
-    %get path of this function
-    ppdir = mfilename('fullpath');
-    %get the root
+function dir = getPrefDir
+%get path of this function
+ppdir = mfilename('fullpath');
+%get the root
+
+%different methods on Windows and Linux
+if strncmp(computer,'PC',2)
+    %On windows
     parts = split(ppdir, filesep);
     dpv_prefdir_p = join(parts(1:end-2),filesep);
     dpv_prefdir = dpv_prefdir_p{1};
-    ans = dpv_prefdir;
+else  % isunix
+    parts = strsplit(ppdir, filesep);
+    dpv_prefdir_p = strjoin(parts(1:end-2),filesep);
+    dpv_prefdir = dpv_prefdir_p;
+    
+end
+dir = dpv_prefdir;
+
 end
