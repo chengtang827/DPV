de0425323a5a74a979941d1e6ea2131d1bb034f5
roger <roger.herikstad@gmail.com>
2017-09-11 16:25:32 +0800

    Adds DirString as argument

--- miscellaneous/getDataDirNew.m
+++ miscellaneous/getDataDirNew.m
@@ -1,8 +1,15 @@
-function dd = getDataDirNew(newlevelname)
+function dd = getDataDirNew(newlevelname,varargin)
 %dd = getDataDirNew(LEVEL) returns the absolute path to LEVEL from the current
 %working directory. For example, calling getDataDirNew('day') from
 %/data/Wiesel/20170714/session01 returns /data/Wiesel/20170714
-  parts = split(pwd, filesep);
+  Args = struct('DirString','');
+  Args = getOptArgs(varargin,Args);
+  if ~isempty(Args.DirString)
+    usedir = Args.DirString;
+  else
+    usedir = pwd;
+  end
+  parts = split(usedir, filesep);
   %remove numbers to get the levelname
   levelname = regexprep(parts{end}, "[0-9]*", '');
   if isempty(levelname) %removed all numbers, so must be day
