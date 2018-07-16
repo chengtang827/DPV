b0e65ab3005ac421d859b82adf81f6d6922f765e
akihumii <a0126587@u.nus.edu>
2018-07-17 01:22:20 +0800

    edited runProcessLevel to separate the splitting task into four without any skip even with the skipping marker

--- runProcessLevel.m
+++ runProcessLevel.m
@@ -4,11 +4,11 @@ function [] = runProcessLevel()
 % 
 %   Detailed explanation goes here
 channelId = [{1:32} ; {33:64} ; {65:96} ; {97:124}];
-
 for i = 1:size(channelId,1)
     ProcessLevel(rplsplit,'skipCheckingMarkers',1,'Levels','Day','SaveLevels',3,...
         'SkipLFP',1,'UseHPC',1,'Channels',channelId{i,1},'skipCheckingRplsplit',1,...
         'HPCCmd','source ~/.bash_profile; qsub $GITHUB_MATLAB/Hippocampus/Compiler/rplsplit/rsHPC_submit_file.txt')
+    pause(5);
 end
 
 end
